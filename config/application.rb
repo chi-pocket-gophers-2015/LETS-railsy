fb36f738aeee3e6711017ac677163cb65b7bd340
cjhaddad & Nathaniel Clapp <pair+cjhaddad.nclapp@devbootcamp.com>
2015-06-02 14:04:51 -0500

    add actionmailer stuff to config file

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module LETS
   class Application < Rails::Application
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: 'letsdecisions@gmail.com',
+      password: 'igeekallweek',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
