29f7934c3f75da170740c502afc57296879f6347
Shannon Staszak <sestaszak@gmail.com>
2015-06-01 22:34:50 -0500

    decision controller plus proposal model methods

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -10,6 +10,26 @@ class Proposal < ActiveRecord::Base
     @_decision ||= self.participation.decision
   end
 
+  def participations
+    @_participations ||= self.decision.participations
+  end
+
+  def voters
+    @_voters ||= self.queries.pluck(:participation_id).sort.map {|id| Participation.find(id)}
+  end
+
+  def not_yet_voted
+    @_not_yet_voted ||= (participations - voters).sort
+  end
+
+  def ordered_participations
+    @_ordered_participations ||= not_yet_voted + voters
+  end
+
+  def next_voter
+    ordered_participations[0]
+  end
+
   validates_presence_of :status, :proposed_idea, :participation_id
   # validates_inclusion_of :status, :in => %w(accepted refused open)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,24 @@ Rails.application.routes.draw do
     get "logout" => "sessions#destroy", :as => "logout"
 
     resources :sessions, only: [:create, :destroy]
-    resources :users, only: [:create ]
+    resources :users, only: [:create, :show]
 
-    resources :users, only: [:create] do
-      resources :decisions, only: [:create, :update, :index, :show]# do
-        # resources :participations, only: [:index]
-      # end
-    end
+    # resources :users, only: [:create] do
+    #   resources :decisions, only: [:create, :update, :index, :show]# do
+    #     # resources :participations, only: [:index]
+    #   # end
+    # end
 
-    resources :decisions, only: [:create, :update, :show] do
-      resources :proposals, only: [:create, :update, :show] do
-        resources :queries, only: [:create, :update]
-      end
-    end
+    # resources :decisions, only: [:create, :update, :show] do
+    #   resources :proposals, only: [:create, :update, :show] do
+    #     resources :queries, only: [:create, :update]
+    #   end
+    # end
 
-    resources :decisions, only: [:index, :show, :create, :update] do
-      resources :participations, only: [:index,:create, :destroy]
-    end
+    # resources :decisions, only: [:index, :show, :create, :update] do
+    #   resources :participations, only: [:index,:create, :destroy]
+    # end
 
     # match '/decisions/:id/activeproposal' => 'decisions#activeproposal', :via => :get, as: "decision_active_proposal"
-    get '/decisions/:id/activeproposal' => 'decisions#activeproposal', as: "decision_active_proposal"
+    # get '/decisions/:id/activeproposal' => 'decisions#activeproposal', as: "decision_active_proposal"
 end
