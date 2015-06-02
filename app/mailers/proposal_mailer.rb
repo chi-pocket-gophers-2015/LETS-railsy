07db4423621b1d022dbe65b816f563f590e3f50c
cjhaddad & Nathaniel Clapp <pair+cjhaddad.nclapp@devbootcamp.com>
2015-06-02 15:24:07 -0500

    add mailer method and message tempalte

--- app/mailers/proposal_mailer.rb
+++ app/mailers/proposal_mailer.rb
@@ -1,2 +1,9 @@
 class ProposalMailer < ApplicationMailer
+
+  def notify_of_turn_to_vote(user)
+    @user = user
+    mail(to: @user.email, subject: "LET'S - It's Your Turn to Vote!")
+  end
+
+
 end
