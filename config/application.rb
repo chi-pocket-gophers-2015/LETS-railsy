3f7067457843c552dbccf9958dc38b19086038a9
cjhaddad <candice.haddad@gmail.com>
2015-06-02 21:25:10 -0500

    added styling to active thread and cleaned up minor things

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module LETS
   class Application < Rails::Application
+
+    # Giphy::Configuration.configure do |config|
+    #   config.version = THE_API_VERSION
+    #   config.api_key = "dc6zaTOxFJmzC"
+    # end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
