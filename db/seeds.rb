2399a127991a227a6fd291433d70d7218a1a445b
Logan Price <logandsprice@gmail.com>
2015-06-03 15:53:02 -0500

    comment out stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-user1 = User.create!(name:"Candice",email:"c@c.cc",password:"password", phone_number:"999949999")
-user2 = User.create!(name:"Shannon",email:"s@s.ss",password:"password", phone_number:"999993999")
-user3 = User.create!(name:"Natty",email:"n@n.nn",password:"password", phone_number:"999999929")
-user4 = User.create!(name:"Logan",email:"l@l.ll",password:"password", phone_number:"999991999")
+# user1 = User.create!(name:"Candice",email:"c@c.cc",password:"password", phone_number:"999949999")
+# user2 = User.create!(name:"Shannon",email:"s@s.ss",password:"password", phone_number:"999993999")
+# user3 = User.create!(name:"Natty",email:"n@n.nn",password:"password", phone_number:"999999929")
+# user4 = User.create!(name:"Logan",email:"l@l.ll",password:"password", phone_number:"999991999")
 
 
-user1.decisions.create!(context:"have dinner tonight!")
+# user1.decisions.create!(context:"have dinner tonight!")
 
-d = Decision.first
+# d = Decision.first
 
-user2.decisions << d
-user3.decisions << d
-user4.decisions << d
+# user2.decisions << d
+# user3.decisions << d
+# user4.decisions << d
 
-part1 = Participation.all[0]
-part2 = Participation.all[1]
+# part1 = Participation.all[0]
+# part2 = Participation.all[1]
 
-part1.proposals.create!(proposed_idea:"DOMINOES", status:"open")
+# part1.proposals.create!(proposed_idea:"DOMINOES", status:"open")
 
-prop1  = Proposal.first
-query1 = prop1.queries.create!(participation:part1,status:"yes")
+# prop1  = Proposal.first
+# query1 = prop1.queries.create!(participation:part1,status:"yes")
 
-query2 = prop1.queries.create!(participation:part2,status:"yes")
+# query2 = prop1.queries.create!(participation:part2,status:"yes")
