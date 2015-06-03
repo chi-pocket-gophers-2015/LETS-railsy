4dc8a081a2237a17495cb031f95582ff0cb253d4
Nathaniel Clapp <github@nattyclapp.com>
2015-06-03 10:42:04 -0500

    write letter templates

--- app/mailers/proposal_mailer.rb
+++ app/mailers/proposal_mailer.rb
@@ -2,22 +2,23 @@ class ProposalMailer < ApplicationMailer
 
   def notify_of_turn_to_vote(user)
     @user = user
-    # @decision = decision
     mail(to: @user.email, subject: "LET'S - It's Your Turn to Vote!")
   end
 
-  def invitation_notification(user)
+  def notify_of_invitation(user)
     @user = user
     mail(to: @user.email, subject: "LET'S - You're Invited!")
   end
 
-  def decision_made_notification(user)
+  def notify_of_final_decision(user)
     @user = user
+    @decision = decision
     mail(to: @user.email, subject: "LET'S - You've Reached a Decision!")
   end
 
-  def time_expired_notification(user)
+  def notify_of_expired_time(user, decision)
     @user = user
+    @decision = decision
     mail(to: @user.email, subject: "LET'S - You Missed Your Turn!")
   end
 
