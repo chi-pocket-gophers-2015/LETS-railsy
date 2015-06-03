1d3b66d3136991733949606318b1ee17c4bc5eb1
Nathaniel Clapp <github@nattyclapp.com>
2015-06-03 12:19:23 -0500

    all mailer methods placed except timeout, not tested

--- app/mailers/proposal_mailer.rb
+++ app/mailers/proposal_mailer.rb
@@ -1,15 +1,18 @@
 class ProposalMailer < ApplicationMailer
 
+  # In QueryService::create_query
   def notify_of_turn_to_vote(user)
     @user = user
     mail(to: @user.email, subject: "LET'S - It's Your Turn to Vote!")
   end
 
+  # In QueryService::add_user
   def notify_of_invitation(user)
     @user = user
     mail(to: @user.email, subject: "LET'S - You're Invited!")
   end
 
+  # In QueryService::close
   def notify_of_final_decision(user, decision)
     @user = user
     @decision = decision
