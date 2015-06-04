807acaefa1bd6a3e766765a91351a16fb87fe837
Logan Price <logandsprice@gmail.com>
2015-06-04 09:02:19 -0500

    edit mailer html and environment to properly assign url

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'http://lets-decide.herokuapp.com' }
