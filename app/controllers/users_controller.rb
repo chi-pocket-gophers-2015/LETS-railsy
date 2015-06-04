cc7f391b0a47625f54dc5bbc2c1599af162a1a33
Nathaniel Clapp <github@nattyclapp.com>
2015-06-04 14:40:36 -0500

    email to downcase in user#create and sessions#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    puts user_params
-    @user = User.new(user_params)
+    @user = User.new(name: user_params[:name], email: user_params[:email].downcase, password: user_params[:password])
+    puts "================================= #{@user.email}"
 
     if @user.save
       session[:user_id] = @user.id
